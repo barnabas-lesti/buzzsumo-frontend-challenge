472882b8b2cb7f85de6886982e59f6f7210a5508
Barnabas Lesti <barnabas.lesti@gmail.com>
2022-08-13 07:36:21 +0200

    Created topics store

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-console.log('App');
+import { useTopicsStore } from './stores/topics';
+const topicsStore = useTopicsStore();
+
+console.log('topicsStore.topics', topicsStore.topics);
 </script>
 
 <template>
-  <div class="App">App</div>
+  <div class="App">
+    Topics:
+    <pre>{{ JSON.stringify(topicsStore.topics) }}</pre>
+  </div>
 </template>
 
 <style lang="scss"></style>
