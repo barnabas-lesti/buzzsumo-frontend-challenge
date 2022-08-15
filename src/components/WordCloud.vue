91ea36fb060abdb37d35fd85c5f1313be0606478
Barnabas Lesti <barnabas.lesti@gmail.com>
2022-08-15 12:37:55 +0200

    Implemented metadata component

--- src/components/WordCloud.vue
+++ src/components/WordCloud.vue
@@ -15,6 +15,8 @@ function onSelect(id: string) {
 
 <template>
   <div class="WordCloud">
+    <h1>My Topics Challenge</h1>
+
     <div v-if="topicsStore.isLoading" class="WordCloud__loaderContainer">
       <BaseLoader />
     </div>
@@ -23,19 +25,50 @@ function onSelect(id: string) {
         :topics="topicsStore.topics"
         :selectedTopicId="topicsStore.selectedTopicId"
         @select="onSelect"
+        class="WordCloud__display"
       />
-      <WordCloudMetaData :topic="topicsStore.selectedTopic" />
+      <WordCloudMetaData
+        v-if="topicsStore.selectedTopic"
+        :topic="topicsStore.selectedTopic"
+        class="WordCloud__metadata"
+      />
     </div>
   </div>
 </template>
 
 <style lang="scss">
 .WordCloud {
+  $metadataWidth: 350px;
+  $gap: 1rem;
+  $chartHeight: 400px;
+
   &__loaderContainer {
     display: flex;
     align-items: center;
     justify-content: center;
-    height: 400px;
+    height: $chartHeight;
+  }
+
+  &__display {
+    margin-bottom: $gap;
+  }
+
+  @media only screen and (min-width: 768px) {
+    &__content {
+      display: flex;
+      justify-content: space-between;
+    }
+
+    &__display {
+      width: calc(100% - $metadataWidth - $gap);
+    }
+
+    &__metadata {
+      width: $metadataWidth;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    }
   }
 }
 </style>
