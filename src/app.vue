3bb9f5faf552c39cd38851d6ddc36aab595d5b3a
Barnabas Lesti <barnabas.lesti@gmail.com>
2022-08-31 10:57:02 +0200

    Added footer and header.

--- src/app.vue
+++ src/app.vue
@@ -3,11 +3,29 @@
  * Main application component.
  */
 
-import WordCloud from './components/word-cloud/word-cloud.vue';
+import { LayoutFooter, LayoutHeader } from './components/layout';
+import { WordCloud } from './components/word-cloud';
 </script>
 
 <template>
   <div class="app">
-    <word-cloud />
+    <layout-header />
+    <div class="content">
+      <word-cloud />
+    </div>
+    <layout-footer />
   </div>
 </template>
+
+<style scoped lang="scss">
+.app {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+
+  .content {
+    flex: 1 0 auto;
+    padding: 0 1rem;
+  }
+}
+</style>
